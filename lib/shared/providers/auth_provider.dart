a66479335c7aef78e65dc51e374410a567333acc
ThomasSAMP <lol.loli@outlook.fr>
2025-06-05 23:50:42 +0200

    remove double unreadAlertsCountProvider

--- lib/shared/providers/auth_provider.dart
+++ lib/shared/providers/auth_provider.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/di/injection.dart';
 import '../../core/services/auth_service.dart';
 import '../../core/utils/logger.dart';
-import '../../features/notifications/providers/medication_alert_provider.dart';
 import '../models/user_model.dart';
 import '../repositories/user_repository.dart';
 
@@ -42,9 +41,4 @@ final isAuthenticatedProvider = Provider<bool>((ref) {
     loading: () => false,
     error: (_, __) => false,
   );
-});
-
-// Provider de compatibilité pour le nombre de notifications non lues
-final unreadNotificationsCountProvider = Provider<int>((ref) {
-  return ref.watch(unreadAlertsCountProvider);
 });
